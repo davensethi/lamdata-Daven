107259842ff90d8cab8437846f792751edf4d7fb
davensethi@gmail.com <davensethi@gmail.com>
2020-09-28 19:36:14 -0400

    this is driving me crazy

--- setup.py
+++ setup.py
@@ -1,28 +1,30 @@
-'lambdata a collection of Data Science Functions'
+"""removing NAN"""
 
-import setuptools
+import setuptools 
+
 
 REQUIRED = [
-    'numpy',
-    'pandas'
+  "numpy",
+  "pandas"
 ]
 
-with  open ('READ.md', 'r') as fh:
-    LONG_DESCRIPTION  = fh.read()
+with open("README.md", "r") as fh:
+  LONG_DESCRIPTION = fh.read()
 
-setuptools.setup (
-    name ='lambdata_daven',
-    version = '0.01',
-    author ='davensethi',
-    description = 'example of the what we are doing in class'
-    Long_description = LONG_DESCRIPTION
-    Long_description_content = 'text/markdown'
-    url= ''
-    packages= setuptools.find_packages(),
-    install_requires =REQUIRED
-    classifiers =[
-        'Programming Language :: Python :: 3'
-        'License :: OSI Approved :: MIT License'
-        'Operating System:: OS Independent'
-    ]
+setuptools.setup(
+  name="lambdata",
+  version="0.0.1",
+  author="nwdelafu",
+  description="A collection of data science functions",
+  long_description=LONG_DESCRIPTION,
+  long_description_content="text/markdown",
+  url='https://github.com/nwdelafu/lambdata',
+  packages=setuptools.find_packages(),
+  python_requires=">=3.6",
+  install_requires=REQUIRED,
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS independent"
+  ]
 )
